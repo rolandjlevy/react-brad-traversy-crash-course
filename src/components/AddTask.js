984690b0ab216c9f3d841f8d5a93d3e50c88f89c
Roland Levy <rolandjlevy@gmail.com>
2021-01-31 23:44:09 +0000

    Created form for AddTask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddTask = () => {
+  const [text, setText] = useState('');
+  const [day, setDay] = useState('');
+  const [reminder, setReminder] = useState(false);
   return (
     <form className='add-form'>
       <div className='form-control'>
         <label>Task</label>
-        <input type="text" placeholder="Add task" />
+        <input 
+          type="text" 
+          value={text} 
+          placeholder="Add task" 
+          onChange={(e) => setText(e.target.value)} 
+        />
       </div>
-      <div className='form-control form-control-check'>
+      <div className='form-control'>
         <label>Day & Time</label>
-        <input type="text" placeholder="Add Day & Time" />
+        <input 
+          type="text" 
+          value={day} 
+          placeholder="Add Day & Time" 
+          onChange={(e) => setDay(e.target.value)} 
+        />
       </div>
       <div className='form-control form-control-check'>
         <label>Set reminder</label>
-        <input type="checkbox" />
+        <input 
+          type="checkbox" 
+          checked={reminder} 
+          value={reminder} 
+          onClick={(e) => setReminder(e.currentTarget.checked)} 
+        />
       </div>
 
-      <input type="submit" value="Save" />
+      <input type="submit" value="Save" className="btn btn-block" />
     </form>
   );
 }
